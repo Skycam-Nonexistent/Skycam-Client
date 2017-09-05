537e936e21a16ad3b650b7128c0a63da3ff0d430
noxoJ <cijophxon@gmail.com>
2017-09-05 23:24:55 +0800

    美化代码

--- app/src/main/java/dev/joxon/skycam/MainActivity.java
+++ app/src/main/java/dev/joxon/skycam/MainActivity.java
@@ -26,6 +26,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private FloatingActionsMenu floatingActionsMenu;
     private static final String DEFAULT_URL = "http://10.206.12.207:8080";
 
     @Override
@@ -46,99 +47,90 @@ public class MainActivity extends AppCompatActivity {
         // Add timeout actions?
         webView.loadUrl(DEFAULT_URL);
 
-        final FloatingActionsMenu floatingActionsMenu = findViewById(R.id.FabMore);
-
+        floatingActionsMenu = findViewById(R.id.FabMore);
         // Input URL
-        findViewById(R.id.ButtonInputUrl).
+        findViewById(R.id.ButtonInputUrl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionsMenu.collapse();
 
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        floatingActionsMenu.collapse();
+                final EditText editText = new EditText(MainActivity.this);
+                final String http = "http://";
+                editText.setSingleLine();
+                editText.setText(http);
+                editText.setSelection(http.length());
 
-                        final EditText editText = new EditText(MainActivity.this);
-                        final String http = "http://";
-                        editText.setSingleLine();
-                        editText.setText(http);
-                        editText.setSelection(http.length());
-
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle(R.string.input_url)
-                                .setView(editText)
-                                .setIcon(android.R.drawable.ic_menu_edit)
-                                .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        webView.loadUrl(editText.getText().toString());
-                                    }
-                                })
-                                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        // cancel
-                                    }
-                                })
-                                .setNeutralButton(R.string.refresh, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        webView.reload();
-                                    }
-                                })
-                                .show();
-                    }
-                });
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.input_url)
+                        .setView(editText)
+                        .setIcon(android.R.drawable.ic_menu_edit)
+                        .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                webView.loadUrl(editText.getText().toString());
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // cancel
+                            }
+                        })
+                        .setNeutralButton(R.string.refresh, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                webView.reload();
+                            }
+                        })
+                        .show();
+            }
+        });
 
         // Auto Trace
-        findViewById(R.id.ButtonAutoTrace).
-
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        floatingActionsMenu.collapse();
-                    }
-                });
+        findViewById(R.id.ButtonAutoTrace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionsMenu.collapse();
+            }
+        });
 
         // Upload pics
-        findViewById(R.id.ButtonUploadPic).
-
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        floatingActionsMenu.collapse();
-                    }
-                });
+        findViewById(R.id.ButtonUploadPic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionsMenu.collapse();
+            }
+        });
 
         // Take screenshots
-        findViewById(R.id.FabShoot).
+        findViewById(R.id.FabShoot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStorageDirectory().getPath() + "/Skycam/";
+                File dir = new File(path);
+                if (!dir.exists()) {
+                    if (!dir.mkdir())
+                        Toast.makeText(MainActivity.this, R.string.mkdir_failed, Toast.LENGTH_SHORT).show();
+                }
 
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String path = Environment.getExternalStorageDirectory().getPath() + "/Skycam/";
-                        File dir = new File(path);
-                        if (!dir.exists()) {
-                            if (!dir.mkdir())
-                                Toast.makeText(MainActivity.this, R.string.mkdir_failed, Toast.LENGTH_SHORT).show();
-                        }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.US);
+                String fname = path + sdf.format(new Date()) + ".png";
 
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.US);
-                        String fname = path + sdf.format(new Date()) + ".png";
-
-                        Bitmap bitmap = webView.getDrawingCache();
-                        if (bitmap != null) {
-                            Log.d("screenshot:", "bitmap got!");
-                            try {
-                                FileOutputStream out = new FileOutputStream(fname);
-                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
-                                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                                Log.d("screenshot", "file " + fname + " output done.");
-                            } catch (Exception e) {
-                                Log.i("Show", e.toString());
-                            }
-                        } else {
-                            Log.d("screenshot", "bitmap is NULL!");
-                        }
+                Bitmap bitmap = webView.getDrawingCache();
+                if (bitmap != null) {
+                    Log.d("screenshot:", "bitmap got!");
+                    try {
+                        FileOutputStream out = new FileOutputStream(fname);
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        Log.d("screenshot", "file " + fname + " output done.");
+                    } catch (Exception e) {
+                        Log.i("Show", e.toString());
                     }
-                });
+                } else {
+                    Log.d("screenshot", "bitmap is NULL!");
+                }
+            }
+        });
     }
 }
