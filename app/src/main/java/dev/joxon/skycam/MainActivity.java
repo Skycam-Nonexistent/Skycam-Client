9be15aa4548d3e4b8720b4d5056e463039b93c55
noxoJ <cijophxon@gmail.com>
2017-09-06 13:51:57 +0800

    added some git proxy tricks comments

--- app/src/main/java/dev/joxon/skycam/MainActivity.java
+++ app/src/main/java/dev/joxon/skycam/MainActivity.java
@@ -23,6 +23,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+// git config --global http.proxy socks5:127.0.0.1:1080
+// git config --global https.proxy socks5:127.0.0.1:1080
+// git config --global --unset http.proxy
+// git config --global --unset https.proxy
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
